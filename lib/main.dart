e1cb2569705027bf7ffbed3118679a5faef9f455
*xscorpionxx* <*almuazenknight1969@gmail.com*>
2021-10-25 01:35:09 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+import 'package:teste/firstpage.dart';
+void main()=> runApp(MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false
+      ,title: "firstapp",
+      home: Firstpage()
+    );
+  }
+}
